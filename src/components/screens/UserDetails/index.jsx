c0c7e2c881559c508c783a42e06931239ca2be55
KolyaSotnichenko <kolya.sotnichenko0811@gmail.com>
2023-11-17 15:46:11 +0200

    fix

--- src/components/screens/UserDetails/index.jsx
+++ src/components/screens/UserDetails/index.jsx
@@ -86,6 +86,7 @@ const UserDetails = () => {
                   </li>
                 ))}
               </ul>
+              <h2>Posts</h2>
             </>
           )}
           {toggleInfo === "albums" && (
@@ -108,6 +109,7 @@ const UserDetails = () => {
                   </li>
                 ))}
               </ul>
+              <h2>Albums</h2>
             </>
           )}
         </div>
